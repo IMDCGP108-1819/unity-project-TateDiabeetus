22ddf3ccf78bb17ece16a39587798d7248ffa849
tate <tatekill@tiscali.co.uk>
2019-01-09 21:11:56 +0000

    new border and death on impact
    player now dies when coliding with a asteroid and replaced boundaries so that it no longer uses coliders but instead stops the player from moving outside of the camera

---
+++ 2D unity game/Assets/Scripts/Damagebycolision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damagebycolision : MonoBehaviour
+{
+    //health for object
+   public int health = 1;
+
+    //colision for object, when collision occurs the health is lowered till it hits 0 and destroys the object
+    private void OnCollisionEnter2D()
+    {
+        Debug.Log("collision");
+
+        health--;
+
+        if(health <= 0)
+        {
+         Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
--- 2D unity game/Assets/Scripts/PlayerController.cs
+++ 2D unity game/Assets/Scripts/PlayerController.cs
@@ -20,6 +20,32 @@ public class PlayerController : MonoBehaviour {
         //inputs so the player can control the sprite using keys
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         moveVelocity = moveInput.normalized * speed;
+
+        Vector2 pos = transform.position;
+
+        // restrict player movement to be in the camera always
+        //roof and floor of screen
+        if(pos.y > Camera.main.orthographicSize)
+        {
+            pos.y = Camera.main.orthographicSize;
+        }
+        if (pos.y < -Camera.main.orthographicSize)
+        {
+            pos.y = -Camera.main.orthographicSize;
+        }
+        //sides of screen
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float widthOrtho = Camera.main.orthographicSize * screenRatio;
+        if (pos.x > widthOrtho)
+        {
+            pos.x = widthOrtho;
+        }
+        if (pos.x < -widthOrtho)
+        {
+            pos.x = -widthOrtho;
+        }
+
+        transform.position = pos;
     }
     void FixedUpdate()
     {
